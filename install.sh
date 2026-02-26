22c87fc551f80132b1829bfc9fad4ac61aeeb4cd
nick <nick@oldFucker.lan>
2026-02-26 17:49:06 +0500

    Rename install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+xbps-install -Suy
+xbps-install git nvim gdb gcc tmux htop timeshift fastfetch nerd-fonts tree 
+xbps-install zsh
+xbps-install zsh-autosuggestions
+xbps-install zsh-completions
+xbps-install zsh-history-substring-search
+xbps-install zsh-syntax-highlighting
+
