1d1f888ab1bc1e0831abf93dda155f512b94ed85
nick <nick@oldFucker.lan>
2026-03-10 15:34:04 +0500

    Add removing old configs for nvim and htop

--- install.sh
+++ install.sh
@@ -51,6 +51,8 @@ mv_configs()
 	cp ./zshrc /etc/zsh/zshrc
 	cp ./rc.conf /etc/rc.conf
 	cp ./rc.local /etc/rc.local 
+	rm -rf /home/$(ls)/.config/nvim
+	rm -rf /home/$(ls)/.config/htop
 	cp -r ./nvim /home/$(ls)/.config/nvim
 	cp -r ./htop /home/$(ls)/.config/htop
 }
